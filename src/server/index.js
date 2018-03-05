c9908d9525886d483206abaf5d74055ef831230c
andrzejglobisz <andrzej_globisz@epam.com>
2018-03-05 13:37:30 +0100

    renaming for convenience - routes

--- src/server/index.js
+++ src/server/index.js
@@ -2,16 +2,16 @@ const Koa = require('koa');
 const indexRoutes = require('./routes/index');
 const usersRoutes = require('./routes/users');
 const userDataRoutes = require('./routes/user_data');
-const beerRoutes = require('./routes/beer');
-const storageRoutes = require('./routes/storage');
+const batchesRoutes = require('./routes/batches');
+const stashesRoutes = require('./routes/stashes');
 
 const app = new Koa();
 const PORT = process.env.PORT || 1337;
 
 app.use(indexRoutes.routes());
 app.use(usersRoutes.routes());
-app.use(beerRoutes.routes());
-app.use(storageRoutes.routes());
+app.use(batchesRoutes.routes());
+app.use(stashesRoutes.routes());
 app.use(userDataRoutes.routes());
 
 const server = app.listen(PORT, () => {
--- src/server/routes/user_data.js
+++ src/server/routes/user_data.js
@@ -1,5 +1,6 @@
 const Router = require('koa-router');
 const queries = require('../db/queries/user_data');
+const common = require('../common/formatOutputData');
 
 const router = new Router();
 const BASE_URL = `/api/v1.0/user_data`;
@@ -8,23 +9,29 @@ router.get(`${BASE_URL}/:user_id`, async (ctx) => {
   try {
     const user_data = await queries.getUserData(ctx.params.user_id);
     let user_batches = await queries.getUserBatches(ctx.params.user_id)
-    let user_stashes = await queries.getUserStorages(ctx.params.user_id);
+    let user_stashes = await queries.getUserStashes(ctx.params.user_id);
 
     let output_data = JSON.parse(JSON.stringify(user_data));
     output_data = Object.assign(...output_data);
 
     user_batches = JSON.parse(JSON.stringify(user_batches));
+
+    let formatted_stashes = [];
     user_stashes = [...JSON.parse(JSON.stringify(user_stashes))];
+    user_stashes.forEach((stash) => {
+      formatted_stashes.push(common.formatSingleStash(stash));
+    });
 
     user_batches.forEach((batch, index) => {
       user_batches[index]['stashes'] = [];
-      user_stashes.forEach((stash) => {
+      formatted_stashes.forEach((stash) => {
         if (batch.batch_number === stash.batch_number) {
           user_batches[index].stashes.push(stash);
         }
       })
     });
-    output_data['storage'] = user_batches;
+    output_data['batches'] = user_batches;
+    
     ctx.body = {
       status: 'success',
       data: output_data
