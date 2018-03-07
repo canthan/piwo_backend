cfc5f56b6509dd473b834fabe6f5ced8b8c9b11f
andrzejglobisz <andrzej_globisz@epam.com>
2018-03-07 16:36:02 +0100

    new seeds - saving db

--- src/server/db/seeds/3_stashes.js
+++ src/server/db/seeds/3_stashes.js
@@ -1,74 +1,8 @@
+const stashesData = require('./3_stashes.json');
 
 exports.seed = (knex, Promise) => {
   return knex('stashes').del()
     .then(() => {
-      return knex('stashes').insert([{
-        stashes_user_id: 1,
-        batch_number: 2,
-        stash_name: "Piwnica",
-        b050: 10,
-        b040: 2,
-        b033: 1,
-      }])
-    })
-    .then(() => {
-      return knex('stashes').insert([{
-        stashes_user_id: 1,
-        batch_number: 2,
-        stash_name: "Dom",
-        b050: 3,
-        b040: 2,
-      }])
-    })
-    .then(() => {
-      return knex('stashes').insert([{
-        stashes_user_id: 1,
-        batch_number: 4,
-        stash_name: "Piwnica",
-        b050: 20,
-        b040: 2,
-        b033: 3,
-      }])
-    })
-    .then(() => {
-      return knex('stashes').insert([{
-        stashes_user_id: 1,
-        batch_number: 4,
-        stash_name: "Dom",
-        b050: 5,
-        b040: 2,
-        b033: 1,
-      }])
-    })
-    .then(() => {
-      return knex('stashes').insert([{
-        stashes_user_id: 1,
-        batch_number: 5,
-        stash_name: "Piwnica",
-        b050: 25,
-        b040: 2,
-        b033: 1,
-      }])
-    })
-    .then(() => {
-      return knex('stashes').insert([{
-        stashes_user_id: 2,
-        batch_number: 4,
-        stash_name: "Dom",
-        b050: 5,
-        b040: 2,
-        b033: 1,
-      }])
-    })
-    .then(() => {
-      return knex('stashes').insert([{
-        stashes_user_id: 2,
-        batch_number: 5,
-        stash_name: "Piwnica",
-        b050: 25,
-        b040: 2,
-        b033: 1,
-      }])
+      return knex('stashes').insert(stashesData);
     });
-   
-  };
+};
